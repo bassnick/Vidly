85975cfa18a9b058f9d0795e59d5032c8bdb5af9
bassnick <tomaspodesva@gmail.com>
2024-06-09 18:22:02 +0200

    Add membership name. changed gui

---
+++ Vidly/Migrations/202406091612031_AddedMembershipName.cs
@@ -0,0 +1,22 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedMembershipName : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "MembershipName", c => c.String(nullable: false));
+            Sql("UPDATE MembershipTypes SET MembershipName = 'Pay as You Go' WHERE SignUpFee = 0");
+            Sql("UPDATE MembershipTypes SET MembershipName = 'Monthly' WHERE SignUpFee = 30");
+            Sql("UPDATE MembershipTypes SET MembershipName = 'Quarterly' WHERE SignUpFee = 90");
+            Sql("UPDATE MembershipTypes SET MembershipName = 'Annual' WHERE SignUpFee = 300");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "MembershipName");
+        }
+    }
+}
